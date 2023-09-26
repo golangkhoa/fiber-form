af6dc0d63f9252c187df7934cc07ee036fc6c485
golangkhoa <145892397+golangkhoa@users.noreply.github.com>
2023-09-26 16:08:48 +0700

    initiate a golang project

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"github.com/gofiber/fiber/v2"
+	// "github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	// engine := html.New("./views", ".tmpl")
+    // app := fiber.New(fiber.Config{
+	// 	Views: engine,
+	// })
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world!")
+    })
+
+	app.Listen(":3000")
 }
