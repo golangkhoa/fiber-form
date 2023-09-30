dc2e1dbfe893fe1ab2c50f9d6ec84c4287bbc6e9
golangkhoa <khoa030634@stu.vinschool.edu.vn>
2023-09-30 13:16:11 +0700

    Add functionality for logging in.

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"	
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -20,14 +22,17 @@ func main() {
 		Views: engine,
 	})
 	db, err := sql.Open("sqlite3", "user.db")
+	readDB, err := gorm.Open(sqlite.Open("user.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
-		})
+		return c.Render("signup", fiber.Map{})
+	})
+
+	app.Get("/login", func(c *fiber.Ctx) error {
+		return c.Render("login", fiber.Map{})
 	})
 
 	app.Post("/", func(c *fiber.Ctx) error {
@@ -37,13 +42,24 @@ func main() {
 
 		insert, _ := db.Prepare("INSERT INTO users (username, password) VALUES (?, ?)")
 		_, err = insert.Exec(user.Username, user.Password)
-		fmt.Println(err)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
-		})
+		return c.Render("signup", fiber.Map{})
 	})
 
+	app.Post("/login", func(c *fiber.Ctx) error {
+		if err := c.BodyParser(&user); err != nil {
+			return err
+		}
+
+		readDB.First(&user, "username = ?", user.Username)
+		readDB.First(&user, "password = ?", user.Password)
+
+		return c.Render("login", fiber.Map{})
+	})
+	
 	app.Listen(":3000")
 	db.Close()
 }
